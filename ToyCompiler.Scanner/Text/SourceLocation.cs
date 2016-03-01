79e241fce45b0a9fb863c066341b90331a66841c
xiaoqiu <wangyg_89@163.com>
2016-03-01 15:01:35 +0800

    创建项目
    首次提交项目代码

---
+++ ToyCompiler.Scanner/Text/SourceLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ToyCompiler.Scanner.Text
+{
+    public class SourceLocation
+    {
+    }
+}
