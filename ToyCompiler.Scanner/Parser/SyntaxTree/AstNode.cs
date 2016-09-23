2b92f345f6025ba2d87cce957e10db07be359835
xiaoqiu <wangyg_89@163.com>
2016-09-24 07:52:42 +0800

    添加抽象结点基类

---
+++ ToyCompiler.Scanner/Parser/SyntaxTree/AstNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyCompiler.Scanner
+{
+    public abstract class AstNode
+    {
+        public SourceLocation StartLocation { get; set; }
+
+        public string Name { get; set; }
+    }
+}
